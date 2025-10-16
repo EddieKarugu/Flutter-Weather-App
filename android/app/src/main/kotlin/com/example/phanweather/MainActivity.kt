448084217ce0befea2c1d99ee07b6d694457bd6c
EddieKarugu <gichukiedwin36@gmail.com>
2025-10-16 23:26:29 +0300

    Weather app Starting point

---
+++ android/app/src/main/kotlin/com/example/phanweather/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.phanweather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:phanweather/Screens/landing_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true),
+      darkTheme: ThemeData.dark(useMaterial3: true),
+      home: LandingScreen(),
+    );
+  }
+}
